aca5f6918d168e8d7af87f50a8b2e80b412a4188
Kiwiguard <frida.susanne@gmail.com>
2020-10-21 14:26:22 +0200

    Added database congfig

---
+++ config/database.php
@@ -0,0 +1,28 @@
+<?php
+
+class Database {
+    /* Database settings */
+    private $host = 'localhost';
+    private $db_name = 'courses';
+    private $username = 'courses_admin';
+    private $password = 'password';
+    private $conn;
+
+    /* Database connection */
+    public function connect() {
+        $this->conn = null;
+
+        try {
+            $this->conn = new PDO('mysqli:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->passaword);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        //Connection error
+        } catch(PDOException $e) {
+            echo "Connection error " . $e->getMessage();
+        }
+        return $this->conn;
+    }
+    public function close() {
+        $this->conn = null;
+    }
+    
+}
