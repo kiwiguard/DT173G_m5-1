a045e132d6fc220dbf888b048c1d44c94e5fc88f
Kiwiguard <frida.susanne@gmail.com>
2020-10-21 14:55:10 +0200

    wrote switch cases for GET, PUT, POST, DELETE

--- api.php
+++ api.php
@@ -3,16 +3,30 @@
 header("Content-Type: application/json; charset=UTF-8");
 header("Acces-Control-Allow-Origin: *"); //Tillåter anrop ifrån alla domäner
 header("Access-Control-Allow-Origin: POST, GET, DELETE, PUT"); //Bestämmer vilka metoder som webbtjänsten tillåter
-header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Access-Control-Allow-Origin, Authorization, X-Requested-with');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Access-Control-Allow-Origin, Authorization, X-Requested-With');
 
 $method = $_SERVER['REQUEST_METHOD']; //Variabel för att lagra medskickad metod
+if(isset($_GET['id'])) {
+    $id = $_GET['id']; // Kontrollerar om id har skickats med i url, sparas i variabel id
+}
 
+//Initiate connection to database
+$databse = new Database();
+$db = $database->connect();
 
+//Skapar instans av klassen för att skicka SQL-frågor till databasen
+//Skickar med databasanslutningen som parameter
+$c = new Courses($db);
 
 switch ($method){
     case "GET": //Get stored classes from database
-        //Kod för get
-        $response = $c->getCourse();
+        if(isset($id)) {
+            $response = $c->readOne($id); //Runs function to read course with matching id
+        } else {
+            $response = $c->read(); //If no id, read all courses
+        }
+
+        //Checks if query returns any data
         if(sizeof($response)>0) {
             http_response_code(200); //OK
         } else {
@@ -22,10 +36,16 @@ switch ($method){
         break;
 
     case "PUT" : 
-        //Kod för put
-        if($c->updateCourse($input['name'], $input['code'], $input['progression'], $input['syllabus'], $input['id'])) {
-            http_response_code(200); //OK
-            $response = array('message' => "Course updated.");
+        if(!isset($id)) {
+            http_response_code(510); //Not Extended
+            $result = array('message' => 'No id sent');
+        } else {
+            $input = json_decode(file_get_contents('php://input'));
+        }
+
+        if ($c->updateCourse($input->name, $input->code, $input->progression, $input->syllabus)) {
+                http_response_code(200); //OK
+                $response = array('message' => "Course updated.");
         } else {
             http_response_code('500');
             $response = array('message' => 'Error updating course.'); //Error message
@@ -33,27 +53,36 @@ switch ($method){
         break;
 
     case "POST" : //Add course to database
-        //Kod för post
-        if($c->addCourse($input['name'], $input['code'], $input['progression'], $input['syllabus'])) {
+        $input = json_decode(file_get_contents('php://input'));
+
+        if($c->addCourse($input->name, $input->code, $input->progression, $input->syllabus)) {
             http_response_code(201); // Created
-            $response = array("message" => "Database updated.");
+            $response = array("message" => "Courses updated.");
         } else {
             http_response_code(500);
             $response = array ("message" => "Error adding course."); //Error message
         }
         break;
 
-    case "DELETE" :
-        //Kod för delete
-        if($c->deleteCourse($input['id'])) {
-            http_response_code(200); //Ok
-            $response = array("message" => "Course deleted.");
+    case "DELETE" : //Delete course from database
+        if(!isset($id)) {
+            http_response_code(510); //Not Extended
+            $result = array('message' => 'No id sent');
         } else {
-            http_response_code(500); //Server error
-            $response = array("message" => "Error deleting course."); //Error message
+            //Run function to delete course
+            if($c->deleteCourse($id)) {
+                http_response_code(200); //Ok
+                $response = array("message" => "Course deleted.");
+            } else {
+                http_response_code(500); //Server error
+                $response = array("message" => "Error deleting course."); //Error message
+            }
         }
         break;
 }
 
-//Returnerar resultat i JSON-format
+//Return result in JSON-format
 echo json_encode($respose);
+
+//Close database connection
+$db = $database->close();
--- classes/classes.php
+++ classes/classes.php
@@ -1,6 +1,7 @@
 <!--
     Metoder för klasserna
-    getCourses => Returnera alla poster i tabellen Classes
+    read => Returnera alla poster i tabellen Classes
+    readOne => Returnerar rad med specifikt ($id)
     addCourse => ($name, $code, $progression, $syllabus)
     deleteCourse => ($id)
     updateCourse => ($name, $code, $progression, $syllabus, $id) -> uppdaterar klass med id=$id
